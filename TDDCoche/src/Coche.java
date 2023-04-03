7b95b4cdc399e989565c296d02e280d14029a3d0
frariqsan <frariqsan@gmail.com>
2023-04-04 01:16:32 +0200

    Refactorized

--- TDDCoche/src/Coche.java
+++ TDDCoche/src/Coche.java
@@ -1,13 +1,14 @@
 
 public class Coche {
+	//REFACTORIZADO
 	public int velocidad;
 
-	public void acelerar(int aceleracion) {
+	public void acelerar_franRiquelme(int aceleracion) {
 		velocidad += aceleracion;
 		
 	}
 
-	public void decelerar(int deceleracion) {
+	public void decelerar_franRiquelme(int deceleracion) {
 		velocidad -= deceleracion;
 		if(velocidad < 0) {
 			velocidad = 0;
--- TDDCoche/src/Tests.java
+++ TDDCoche/src/Tests.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import junit.framework.Assert;
 
 class Tests {
-	// TODOS LOS TESTS REALIZADOS
+	// REFACTORIZADO
 	@Test
 	public void test_al_crear_un_coche_su_velocidad_es_cero() {
 		Coche nuevoCoche = new Coche();
@@ -15,14 +15,14 @@ class Tests {
 	@Test
 	public void test_al_acelerar_un_coche_su_velocidad_aumenta() {
 		Coche nuevoCoche = new Coche();
-		nuevoCoche.acelerar(30);
+		nuevoCoche.acelerar_franRiquelme(30);
 		Assert.assertEquals(30, nuevoCoche.velocidad);
 	}
 	@Test
 	public void test_al_decelerar_un_coche_su_velocidad_no_puede_ser_menor_que_cero() {
 		Coche nuevoCoche = new Coche();
 		nuevoCoche.velocidad = 50;
-		nuevoCoche.decelerar(80);
+		nuevoCoche.decelerar_franRiquelme(80);
 		Assert.assertEquals(0, nuevoCoche.velocidad);
 	}
 
